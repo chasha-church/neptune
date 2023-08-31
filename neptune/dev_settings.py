9f973860d928ae4fef1755d1acdcb2a2a0609888
Pavel Piasetski <pyasecky2012pavel@gmail.com>
2023-08-31 17:51:17 +0300

    Add dev_settings.py

--- neptune/dev_settings.py
+++ neptune/dev_settings.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 
+import pymysql
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 DEBUG = True
@@ -9,7 +11,17 @@ ENVIRONMENT_NAME = 'Devs local'
 
 ALLOWED_HOSTS = []
 
+pymysql.install_as_MySQLdb()
+
 DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        'TEST': {
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    },
+
     'develop': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ.get('DEVELOP_NAME'),
@@ -17,10 +29,5 @@ DATABASES = {
         'PASSWORD': os.environ.get('DEVELOP_PASSWORD'),
         'HOST': os.environ.get('DEVELOP_HOST'),
         'PORT': '3306',
-    },
-
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
