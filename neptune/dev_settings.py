87402fc57898c8c3344f9fa117f760201aead236
Pavel Piasetski <pyasecky2012pavel@gmail.com>
2023-09-08 19:05:16 +0300

    CC-24 update settings files

--- neptune/dev_settings.py
+++ neptune/dev_settings.py
@@ -1,7 +1,5 @@
-import os
 from pathlib import Path
 
-import pymysql
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -11,26 +9,6 @@ ENVIRONMENT_NAME = 'Devs local'
 
 ALLOWED_HOSTS = ['*']
 
-pymysql.install_as_MySQLdb()
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'TEST': {
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    },
-
-    'develop': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DEVELOP_NAME'),
-        'USER': os.environ.get('DEVELOP_USER'),
-        'PASSWORD': os.environ.get('DEVELOP_PASSWORD'),
-        'HOST': os.environ.get('DEVELOP_HOST'),
-        'PORT': '3306',
-    }
-}
 
 try:
     from .settings import *
