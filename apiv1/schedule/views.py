75d53f89d2c9064301ee2cc9ab174773a0a809e0
Pavel Piasetski <pavel.piasetski@sugarcrm.com>
2024-02-09 18:10:14 +0300

    CC-64 Add CORS to schedule endpoint

--- apiv1/schedule/views.py
+++ apiv1/schedule/views.py
@@ -1,16 +1,17 @@
 from datetime import datetime
 
+from django.conf import settings
 from django.utils import timezone
 from rest_framework import status
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
-from apiv1 import generics
 from apiv1.mixins import RequestArgMixin
 from apiv1.pagination import DefaultPageNumberPagination
 from apiv1.queryables import Queryable
 from apiv1.schedule.serializers import ScheduleOnDayListSerializer
 from apiv1.schedule.services import ScheduleService
+from tools.cors.decorators import allow_cors, allow_cors_methods, cors_max_age
 
 
 class ScheduleOnThisWeekList(GenericAPIView, Queryable, RequestArgMixin):
@@ -20,6 +21,9 @@ class ScheduleOnThisWeekList(GenericAPIView, Queryable, RequestArgMixin):
     def get_queryset(self):
         return None
 
+    @allow_cors(settings.CORS)
+    @allow_cors_methods(settings.CORS_METHODS)
+    @cors_max_age(settings.CORS_MAX_AGE)
     def get(self, request, *args, **kwargs):
         week = self.get_argument('week', required=False, arg_type=int)
         schedule_service = ScheduleService()
