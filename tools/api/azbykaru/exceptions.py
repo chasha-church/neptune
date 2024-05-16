bac252787a6511788700c6d732e21de2fc92db7c
PavelPyasecky <49077394+PavelPyasecky@users.noreply.github.com>
2024-05-16 11:51:14 +0300

    CC2-66 Create a state logic and apply to the News page (#6)
    * CC2 Set status code 500 to Azbuka.ru errors
    
    * CC2-66 Add logging to the app
    
    ---------
    
    Co-authored-by: Pavel Piasetski <pavel.piasetski@sugarcrm.com>

--- tools/api/azbykaru/exceptions.py
+++ tools/api/azbykaru/exceptions.py
@@ -3,10 +3,10 @@ from rest_framework.exceptions import APIException
 
 
 class AzbykaruCredentialsError(APIException):
-    status_code = status.HTTP_400_BAD_REQUEST
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
     default_detail = 'Bad credentials.'
 
 
 class AzbykaruConnectionError(APIException):
-    status_code = status.HTTP_400_BAD_REQUEST
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
     default_detail = 'The azbyka.ru platform is unavailable'
